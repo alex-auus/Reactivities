14f3a7145f15e18a77fa8320f74081ced16223dc
Ali Almaktoum <ali.almaktoum@gmail.com>
2020-09-27 14:00:27 +1000

    Initial commit

---
+++ Persistence/Migrations/20200926075053_SeedValuesModified.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValuesModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Value 101");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value 102");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 103");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Value101");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value102");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value103");
+        }
+    }
+}
